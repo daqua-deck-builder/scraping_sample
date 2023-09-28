7f94ccf9042754b6e440e4a205f64ecbc6e0dfb4
kotobukid <kotobuki@daqua.jp>
2023-09-28 18:38:17 +0900

    ビット演算用の変数名はbitよりshiftであるべき　ということを反映させていたら、事前に計算済みの値をjson化するべきでもあることに気づいたので適用

--- examples/macro_challenge.rs
+++ examples/macro_challenge.rs
@@ -2,7 +2,7 @@
 use serde_json;
 
 macro_rules! def_features {
-    ($group:ident, [$(($name:ident, $desc:expr, $bit:expr)),*$(,)?]) => {
+    ($group:ident, [$(($name:ident, $desc:expr, $shift:expr)),*$(,)?]) => {
         #[derive(Clone, PartialEq, Eq, Hash, Debug)]
         pub enum $group {
             $(
@@ -24,7 +24,7 @@ macro_rules! def_features {
             pub fn to_bit(&self) -> u32 {
                 match self {
                     $(
-                        $group::$name => 1 << $bit,
+                        $group::$name => 1 << $shift,
                     )*
                 }
             }
@@ -33,7 +33,7 @@ macro_rules! def_features {
                 $(
                     let mut inner_map = serde_json::Map::new();
                     inner_map.insert("label".to_string(), serde_json::Value::String($desc.to_string()));
-                    inner_map.insert("b".to_string(), serde_json::Value::Number(serde_json::Number::from($bit)));
+                    inner_map.insert("b".to_string(), serde_json::Value::Number(serde_json::Number::from(1 << $shift)));
                     map.insert(stringify!($name).to_string(), serde_json::Value::Object(inner_map));
                 )*
                 serde_json::Value::Object(map)
@@ -46,6 +46,9 @@ def_features!(CardFeature1, [
     (DoubleCrush, "ダブルクラッシュ", 0),
     (TripleCrush, "トリプルクラッシュ", 1),
     (DiscardOpponent, "手札破壊", 2),
+    (OtherFeature, "手札破壊", 3),
+    (AnotherFeature, "手札破壊", 4),
+    (MoreAnotherFeature, "手札破壊", 5),
 ]);
 
 def_features!(CardFeature2, [
