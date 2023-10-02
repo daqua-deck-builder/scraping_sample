9db93e37ded83fbba42b97aceb22ab1eee97ca2b
kotobukid <kotobuki@daqua.jp>
2023-10-02 14:43:14 +0900

    AxumでHelloWorld ドキュメントコメントのものは動作したが、examplesのものは記述が古く動作しない模様

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
 }
