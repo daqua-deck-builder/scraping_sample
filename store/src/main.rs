5b0e96451845af1eb6dae398920e54d7c8d46067
kotobukid <kotobuki@daqua.jp>
2023-10-03 17:07:37 +0900

    思い出したようにSerialize

--- store/src/main.rs
+++ store/src/main.rs
@@ -1,12 +1,39 @@
 mod schema;
 
+use std::fmt::{Display, Formatter};
 use tokio;
 use sqlx::{Error, FromRow, Postgres, postgres::{PgPoolOptions}};
+use serde::Serialize;
+use serde_json;
 
-#[derive(Debug, FromRow)]
+#[derive(Debug, FromRow, Serialize)]
 struct Card {
     id: i32,
     name: String,
+}
+
+#[derive(Serialize)]
+struct Cards {
+    cards: Vec < Card>
+}
+
+impl Cards {
+    fn new(cards: Vec<Card>) -> Self {
+        Self {
+            cards
+        }
+    }
+}
+
+impl Display for Cards {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+
+        let s: &String = &self.cards.iter().map(|c| {
+            String::from(format!("{} {}\n", c.id, c.name))
+        }).collect();
+        write!(f, "{}", s).unwrap();
+        Ok(())
+    }
 }
 
 struct CardManager {
@@ -35,8 +62,8 @@ async fn main() {
     let pool: sqlx::Pool<Postgres> = pool.unwrap();
     let mut card_manager = CardManager { pool };
 
-    let cards = card_manager.all().await.unwrap();
-    cards.iter().for_each(|c| {
-        println!("Hello, {:?}", c);
-    })
+    let cards = Cards::new(card_manager.all().await.unwrap());
+
+    println!("{}", serde_json::to_string(&cards).unwrap());
+    println!("{}", cards)
 }
