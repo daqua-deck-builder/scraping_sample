91d5b38bff5b79c4ce5264644aad9e347a755ac5
kotobukid <kotobuki@daqua.jp>
2023-10-04 16:04:20 +0900

    CRUD

--- store/src/main.rs
+++ store/src/main.rs
@@ -1,6 +1,7 @@
 mod schema;
 
 use std::fmt::{Display, Formatter};
+use diesel::sql_types::Bool;
 use tokio;
 use sqlx::{ColumnIndex, Error, FromRow, Postgres, postgres::{PgPoolOptions}};
 use serde::{Deserialize, Serialize};
@@ -65,6 +66,45 @@ impl CardManager {
             .await?;
         Ok(value)
     }
+
+    async fn update(&mut self, card: Card) -> Result<Card, sqlx::Error> {
+        println!("[update]");
+
+        let value: Card = sqlx::query_as("update card set name = $1 where id = $2 returning id, name;")
+            .bind(&card.name)
+            .bind(&card.id)
+            .fetch_one(&self.pool)
+            .await?;
+        Ok(value)
+    }
+
+    async fn get(&self, id: i32) -> Result<Card, sqlx::Error> {
+        println!("[get]");
+
+        let user: Card = sqlx::query_as("select id, name from card where id = $1;")
+            .bind(id)
+            .fetch_one(&self.pool)
+            .await?;
+        Ok(user)
+    }
+
+    async fn delete(&self, id: i32) -> Result<i32, sqlx::Error> {
+        println!("[delete]");
+        let result: u64 = sqlx::query("delete from card where id = $1;")
+            .bind(id)
+            .execute(&self.pool)
+            .await?
+            .rows_affected();
+
+        match result {
+            0 => {
+                Ok(0)
+            },
+            _ => {
+                Ok(id)
+            }
+        }
+    }
 }
 
 #[tokio::main]
@@ -78,11 +118,23 @@ async fn main() {
     let mut card_manager = CardManager { pool };
 
     let new_card: NewCard = NewCard {name: "BBB".into()};
-    let new_card = card_manager.create(new_card).await;
-    println!("{:?}", new_card.unwrap());
+    let new_card = card_manager.create(new_card).await.unwrap();
+    println!("{:?}", new_card);
 
     let cards = Cards::new(card_manager.all().await.unwrap());
 
     println!("{}", serde_json::to_string(&cards).unwrap());
-    println!("{}", cards)
+    println!("{}", cards);
+
+    let card_inserted: Card = card_manager.get(new_card.id).await.unwrap();
+    println!("inserted: {:?}", card_inserted);
+
+    let deleted_id = card_manager.delete(card_inserted.id).await.unwrap();
+    println!("deleted: {}", deleted_id);
+
+    let mut card = card_manager.get(1).await.unwrap();
+    card.name = format!("{} {}", card.name, card.name).into();
+    let card_updated = card_manager.update(card).await.unwrap();
+    println!("updated: {:?}", card_updated)
+
 }
